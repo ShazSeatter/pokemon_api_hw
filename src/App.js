c78417acfbac5b17916712818a2cf11fa61ee119
Shazreen Seatter <shazseatter12@gmail.com>
2023-03-10 16:48:11 +0000

    added logo and title, added google fonts and some styling added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'; 
+import React from 'react';
+import pokeball from "./img/pokeball.png" 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <img src={pokeball}/>
+        <h1>Gotta Catch'em All</h1>
       </header>
     </div>
   );
