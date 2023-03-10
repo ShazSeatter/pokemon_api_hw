54a8847f128955ca6768fe484a6d94013a78711b
Shazreen Seatter <shazseatter12@gmail.com>
2023-03-10 16:55:58 +0000

    added footer and styled so its sitting at the bottom:

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <img src={pokeball}/>
         <h1>Gotta Catch'em All</h1>
       </header>
+      <footer>
+      <a href="https://www.flaticon.com/free-icons/pokemon" title="pokemon icons">Pokemon icons created by Nikita Golubev - Flaticon</a>
+      </footer>
     </div>
   );
 }
