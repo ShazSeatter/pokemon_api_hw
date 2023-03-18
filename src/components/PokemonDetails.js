db0c729703ebee04d810c809cbcb53d4872d9b5c
Shazreen Seatter <shazseatter12@gmail.com>
2023-03-18 22:15:18 +0000

    saved

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -15,10 +15,12 @@ const PokemonDetails = function ({pokemon, capitalize}) {
         <>
         <div className="pokemon-detail">
             <div className="pokemon-card">
-            <h2>{capitalize(pokemon.name)}</h2> 
-            <img className="pokemon-img" src={pokemon.sprites.front_default} alt="pokemon" />
-            <p>HEALTH HP: {pokemon.stats[0].base_stat}</p>
+            <div>
+                <h2>{capitalize(pokemon.name)}</h2> 
+                <img className="pokemon-img" src={pokemon.sprites.front_default} alt="pokemon" />
+            </div>
             <section>
+            <p>HEALTH HP: {pokemon.stats[0].base_stat}</p>
             <h4 className="abilites-title">Abilites</h4>
             <div className="pokemon-abilities-list">
                 {pokemonAbilites}
